2f34abb3d4139ad647bd55479727c9f7cc69b966
herreraleon14 <herreraleon14@gmail.com>
2026-02-01 23:22:06 +0000

    Cambios de db

--- db.ts
+++ db.ts
@@ -1,20 +1,15 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
 
 const client = new MongoClient();
-await client.connect({
-    db: "organiza",
-    tls: true,
-    servers: [
-        { host: "organiza-shard-00-01.xen4l.mongodb.net", port: 27017 },
-        { host: "organiza-shard-00-02.xen4l.mongodb.net", port: 27017 },
-        { host: "organiza-shard-00-03.xen4l.mongodb.net", port: 27017 },
-    ],
-    credential: {
-        username: "admin1",
-        password: "admin1",
-        db: "admin",
-        mechanism: "SCRAM-SHA-1",
-    },
-});
+
+// Es mucho mejor usar la variable de entorno, pero si lo pones directo:
+const URI = "mongodb+srv://admin1:admin1@organiza.xen4l.mongodb.net/brasilius?authMechanism=SCRAM-SHA-1";
+
+try {
+  await client.connect(URI);
+  console.log("Conectado a MongoDB Atlas");
+} catch (error) {
+  console.error("Error conectando:", error);
+}
 
 export const db = client.database("brasilius");
