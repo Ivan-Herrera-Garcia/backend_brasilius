31b243a68b3b23b9cfb7d637ed7a87db47f9ec27
Ivan Herrera (Bellkhen) <71898783+Ivan-Herrera-Garcia@users.noreply.github.com>
2026-02-01 14:22:11 -0600

    Add files
    Add files

---
+++ db.ts
@@ -0,0 +1,20 @@
+import { MongoClient } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
+
+const client = new MongoClient();
+await client.connect({
+  db: "organiza",
+  tls: true,
+  servers: [
+    { host: "organiza-shard-00-01.xen4l.mongodb.net", port: 27017 },
+    { host: "organiza-shard-00-02.xen4l.mongodb.net", port: 27017 },
+    { host: "organiza-shard-00-03.xen4l.mongodb.net", port: 27017 },
+  ],
+  credential: {
+    username: "admin1",
+    password: "admin1",
+    db: "admin",
+    mechanism: "SCRAM-SHA-1",
+  },
+});
+
+export const db = client.database("brasilius");
